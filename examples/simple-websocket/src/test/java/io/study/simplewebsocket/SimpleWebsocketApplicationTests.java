c7bbba0554d871e9931e9b3e3bdf5036fef6088e
soon.good <playmymind@daum.net>
2021-09-12 03:51:42 +0900

    첫번째 예제 프로젝트 추가

---
+++ examples/simple-websocket/src/test/java/io/study/simplewebsocket/SimpleWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.simplewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
